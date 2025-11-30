5ef000b59a3fec60616a4f76e28dff8a61e29c29
vbxcoder <vbdeveloper@windowslive.com>
2025-11-30 22:34:13 +0000

    new start init

--- AuthSLN/AuthenticationService/Models/DTOs/LoginRequest.cs
+++
@@ -1,8 +0,0 @@
-namespace AuthenticationService.Models.DTOs
-{
-    public class LoginRequest
-    {
-        public string? Email { get; set; }
-        public string? Password { get; set; }
-    }
-}
--- AuthSLN/AuthenticationService/Models/DTOs/SignUpRequest.cs
+++
@@ -1,9 +0,0 @@
-namespace AuthenticationService.Models.DTOs
-{
-    public class SignUpRequest
-    {
-        public string? Name { get; set; }
-        public string? Email { get; set; }
-        public string? Password { get; set; }
-    }
-}
